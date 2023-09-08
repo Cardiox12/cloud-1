b8eb05954032dc7a16114a26cf31bc023fd46e20
mvovantao <mvovantao@scaleway.com>
2023-09-08 15:51:21 +0200

    feat: add output and access policy

--- infra/instance.tf
+++ infra/instance.tf
@@ -4,7 +4,7 @@ resource "scaleway_instance_volume" "data" {
     type = "l_ssd"
 }
 
-resource "scaleway_instance_server" "my-instance" {
+resource "scaleway_instance_server" "cloud-1-instance" {
     type  = "DEV1-S"
     image = "docker"
     name = "cloud-1"
@@ -13,4 +13,22 @@ resource "scaleway_instance_server" "my-instance" {
     root_volume {
       size_in_gb = 10
     }
+    security_group_id = scaleway_instance_security_group.cloud-1-security-group.id
   }
+
+resource "scaleway_instance_security_group" "cloud-1-security-group" {
+  inbound_default_policy  = "drop"
+  outbound_default_policy = "accept"
+  inbound_rule {
+    action = "accept"
+    port   = "22"
+  }
+  inbound_rule {
+    action = "accept"
+    port   = "80"
+  }
+  inbound_rule {
+    action = "accept"
+    port   = "443"
+  }
+}
